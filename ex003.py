192cef244f37e3bff9ae7c793c9ba78b12050315
Josué Vitor Pereira Matos <josuepmatosjj@gmail.com>
2025-07-09 20:04:12 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,3 +1,4 @@
+#float está definindo a variavel como valor decimal
 n1 = float(input("Digite um número"))
 n2 = float(input("Digite outro número"))
 print ("A soma entre {} e {} é {}".format(n1, n2, n1+n2))
