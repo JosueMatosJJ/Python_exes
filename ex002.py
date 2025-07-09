fb5187f9b382af7fe7b893ea9dc0a2b533257cbc
Josué Vitor Pereira Matos <josuepmatosjj@gmail.com>
2025-07-09 20:02:34 -0300

    Update ex002.py

--- ex002.py
+++ ex002.py
@@ -1,3 +1,5 @@
+#input coleta dados do usuario
 nome = (input('Digite seu nome: '))
+#format insere valores de forma dinamica ao que lhe for atribuido
 print ("É um prazer te conhecer,{}!".format(nome))
 
