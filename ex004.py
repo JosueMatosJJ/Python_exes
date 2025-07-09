4988c5d1896d573bc17dec03f6f868aa7dd90a67
Josué Vitor Pereira Matos <josuepmatosjj@gmail.com>
2025-07-09 20:05:35 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,4 +1,4 @@
 a = input('Digite algo')
-print ( ' o tipo primitivo dessa classe é ', type(a))
-print ('só tem espaço ?' ,a.isspace())
-print ('é um numero ?', a.isnumeric())
+print('O tipo primitivo dessa classe é', type(a))      # Exibe o tipo da variável (sempre 'str' ao usar input)
+print('Só tem espaço?', a.isspace())                    # Verifica se a string contém apenas espaços
+print('É um número?', a.isnumeric())                    # Verifica se a string contém apenas dígitos numéricos
